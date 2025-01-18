88a26bdc05e83cf4c700460d5f4cc8da2f1f8c74
Anzar Shah <anzarshah43@gmail.com>
2025-01-18 14:25:23 -0600

    cicd added

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-__version__ = "0.0.0"
+__version__ = "1.0.0"
 
 REPO_NAME = "End-to-end-ML-Project-with-MLflow"
 AUTHOR_USER_NAME = "entbappy"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "anzarshah43@gmail.com"
 
 
 setuptools.setup(
